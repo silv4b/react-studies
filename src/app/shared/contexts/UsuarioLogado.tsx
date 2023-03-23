bdcffa811f23792e7dc2cd959b9437f12355e84f
Bruno Silva <brunosilva@viggosistemas.com.br>
2023-03-22 21:30:10 -0300

    feat: passando state em um context

--- src/app/shared/contexts/UsuarioLogado.tsx
+++ src/app/shared/contexts/UsuarioLogado.tsx
@@ -1,4 +1,4 @@
-import { createContext, useCallback } from "react";
+import { createContext, useCallback, useEffect, useState } from "react";
 
 interface IUsuarioLogadoContextData {
   nomeDoUsuario: string;
@@ -11,12 +11,20 @@ interface IUsuarioLogadoProviderProps {
 }
 export const UsuarioLogadoProvider: React.FC<IUsuarioLogadoProviderProps> = ({ children }) => {
 
+  const [nome, setNome] = useState('');
+
+  useEffect(() => {
+    setTimeout(() => {
+      setNome("Bruno");
+    }, 1000);
+  });
+
   const handleLogout = useCallback(() => {
     console.log('Logout executou');
   }, []);
 
   return(
-    <UsuarioLogadoContext.Provider value={{ nomeDoUsuario: '💖', logout: handleLogout }}> {/* valor inicial do contexto */}
+    <UsuarioLogadoContext.Provider value={{ nomeDoUsuario: nome, logout: handleLogout }}> {/* valor inicial do contexto */}
       {children}
     </UsuarioLogadoContext.Provider>
   );
