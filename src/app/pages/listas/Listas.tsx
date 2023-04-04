a87537cd4c4bba68e56a20a43a5d86c5f1c5eece
Bruno Silva <brunosilva@viggosistemas.com.br>
2023-04-03 21:06:32 -0300

    feat: adicinando check e viricando seu valor dentro do projeto

--- src/app/pages/listas/Listas.tsx
+++ src/app/pages/listas/Listas.tsx
@@ -7,7 +7,6 @@ interface IItemLista {
 }
 
 export const Listas = () => {
-
   const [lista, setLista] = useState<IItemLista[]>([]);
 
   const navigate = useNavigate();
@@ -21,44 +20,65 @@ export const Listas = () => {
   };
 
   // o evento que o useCallback receber é um keyboard event handler tipado como input element (onKeyDown)
-  const handleInputKeyDown: React.KeyboardEventHandler<HTMLInputElement> = useCallback((evt) => {
-    if (evt.key === 'Enter') {
-      if (evt.currentTarget.value.trim().length === 0 ) return;
+  const handleInputKeyDown: React.KeyboardEventHandler<HTMLInputElement> =
+    useCallback((evt) => {
+      if (evt.key === "Enter") {
+        if (evt.currentTarget.value.trim().length === 0) return;
 
-      const value = evt.currentTarget.value;
-      evt.currentTarget.value = '';
+        const value = evt.currentTarget.value;
+        evt.currentTarget.value = "";
 
-      setLista((oldLista) => {
-        // se o valor já existir não inserir e retorna a lista atual
-        if (oldLista.some((ListItem) => ListItem.title === value)) return oldLista;
-        return [
-          ...oldLista,
-          {
-            title: value,
-            isSelected: false
-          }];
-      });
-    }
-  }, []);
+        setLista((oldLista) => {
+          // se o valor já existir não inserir e retorna a lista atual
+          if (oldLista.some((ListItem) => ListItem.title === value))
+            return oldLista;
+          return [
+            ...oldLista,
+            {
+              title: value,
+              isSelected: false,
+            },
+          ];
+        });
+      }
+    }, []);
 
-  return(
+  return (
     <div>
-
-      <button onClick={ handleClique }> Página Inicial </button>
+      <button onClick={handleClique}> Página Inicial </button>
       <h3>Listas</h3>
       <p>Nomes:</p>
-      <input onKeyDown={ handleInputKeyDown } />
+      <input onKeyDown={handleInputKeyDown} />
       &nbsp;
-      <button onClick={ handleCleanList }> Limpar </button>
-
+      <button onClick={handleCleanList}> Limpar </button>
+      <p>{lista.filter((ListItem) => ListItem.isSelected).length}</p>
       <ul>
         {lista.map((ListItem) => {
           return (
-            <li key={ListItem.title}> {ListItem.title} </li>
+            <li key={ListItem.title}>
+              <input
+                type="checkbox"
+                checked={ListItem.isSelected}
+                onChange={() => {
+                  setLista((oldLista) => {
+                    return oldLista.map((oldListItem) => {
+                      const newIsSelected =
+                        oldListItem.title === ListItem.title
+                          ? !oldListItem.isSelected
+                          : oldListItem.isSelected;
+                      return {
+                        ...oldListItem,
+                        isSelected: newIsSelected,
+                      };
+                    });
+                  });
+                }}
+              />
+              {ListItem.title}
+            </li>
           );
         })}
       </ul>
-
     </div>
   );
 };
