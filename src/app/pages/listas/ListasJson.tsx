4af38c2222d1520e268b9d4e8dce38eded0c27cd
Bruno Silva <brunosilva@viggosistemas.com.br>
2023-04-14 16:45:03 -0300

    feat: adicionando botão para remover uma tarefa da api e atualizar o state da lista local

--- src/app/pages/listas/ListasJson.tsx
+++ src/app/pages/listas/ListasJson.tsx
@@ -1,9 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { useCallback, useEffect, useState } from "react";
-import {
-  ITarefa,
-  TarefasService,
-} from "../../shared/services/api/tarefas/TarefasService";
+import { ITarefa, TarefasService } from "../../shared/services/api/tarefas/TarefasService";
 import { APIException } from "../../shared/services/api/APIException";
 
 export const ListasJson = () => {
@@ -30,36 +27,33 @@ export const ListasJson = () => {
   };
 
   // o evento que o useCallback receber é um keyboard event handler tipado como input element (onKeyDown)
-  const handleInputKeyDown: React.KeyboardEventHandler<HTMLInputElement> =
-    useCallback(
-      (evt) => {
-        if (evt.key === "Enter") {
-          if (evt.currentTarget.value.trim().length === 0) return;
+  const handleInputKeyDown: React.KeyboardEventHandler<HTMLInputElement> = useCallback(
+    (evt) => {
+      if (evt.key === "Enter") {
+        if (evt.currentTarget.value.trim().length === 0) return;
 
-          const value = evt.currentTarget.value;
-          evt.currentTarget.value = "";
+        const value = evt.currentTarget.value;
+        evt.currentTarget.value = "";
 
-          if (lista.some((ListItem) => ListItem.title === value)) return;
+        if (lista.some((ListItem) => ListItem.title === value)) return;
 
-          TarefasService.create({ title: value, isCompleted: false }).then(
-            (result) => {
-              if (result instanceof APIException) {
-                alert("APIException: " + result.message);
-              } else {
-                setLista((oldLista) => [...oldLista, result]);
-              }
-            }
-          );
-        }
-      },
-      [lista]
-    );
+        TarefasService.create({ title: value, isCompleted: false }).then((result) => {
+          if (result instanceof APIException) {
+            alert("APIException: " + result.message);
+          } else {
+            setLista((oldLista) => [...oldLista, result]);
+          }
+        });
+      }
+    },
+    [lista]
+  );
 
   const handleToggleComplete = useCallback(
     (id: number) => {
       const tarefaToUpdate = lista.find((tarefa) => tarefa.id === id);
 
-      if (!tarefaToUpdate) return; // se n
+      if (!tarefaToUpdate) return; // se n achar a tarefa pra atualizar (undefined) retorna nada
 
       // alteracao na API
       TarefasService.updateById(id, {
@@ -82,6 +76,20 @@ export const ListasJson = () => {
     [lista]
   );
 
+  const handleDelete = useCallback((id: number) => {
+    // alteracao na API
+    TarefasService.deteleById(id).then((result) => {
+      if (result instanceof APIException) {
+        alert("APIException: " + result.message);
+      } else {
+        // se der certo, altera o state da lista local, filtrando a lista, removendo a tarefa deletada
+        setLista((oldListaTarefa) => {
+          return oldListaTarefa.filter((oldListaTarefa) => oldListaTarefa.id !== id);
+        });
+      }
+    });
+  }, []);
+
   return (
     <div>
       <button onClick={handleClique}> Página Inicial </button>
@@ -100,7 +108,8 @@ export const ListasJson = () => {
                 checked={ListaTarefa.isCompleted}
                 onChange={() => handleToggleComplete(ListaTarefa.id)}
               />
-              {ListaTarefa.title}
+              Id : {ListaTarefa.id} - Descrição : {ListaTarefa.title}{" "}
+              <button onClick={() => handleDelete(ListaTarefa.id)}> x </button>
             </li>
           );
         })}
